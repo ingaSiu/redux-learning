caa57334bfc14acb9d180a2d8e2de95e63ce3dc1
ingaSiu <ingasiud@gmail.com>
2023-12-18 15:18:50 +0200

    pass objects instead of strings

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -2,18 +2,18 @@ import { addTask, completeTask, fetchTodo, removeTask } from './store/tasks';
 
 import store from './store/configureStore';
 
-const unsubscribe = store.subscribe(() => {
-  console.log('Updated', store.getState());
-});
+// const unsubscribe = store.subscribe(() => {
+//   console.log('Updated', store.getState());
+// });
 
-store.dispatch(addTask('Task 1'));
-store.dispatch(addTask('Task 2'));
+store.dispatch(addTask({ task: 'Task 1' }));
+store.dispatch(addTask({ task: 'Task 2' }));
 console.log(store.getState());
 
 // unsubscribe();
-store.dispatch(completeTask(2));
+store.dispatch(completeTask({ id: 2 }));
 
-store.dispatch(removeTask(1));
+store.dispatch(removeTask({ id: 1 }));
 store.dispatch(fetchTodo());
 console.log(store.getState());
 
