4ca3da017457c21180c828aa55db22ea8237c794
ingaSiu <ingasiud@gmail.com>
2023-12-19 10:48:36 +0200

    fetch tasks from api with axios

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,5 +1,18 @@
-import { addTask, completeTask, removeTask } from './store/tasks';
-
-import { addEmployee } from './store/employess';
+import axios from 'axios';
+import { getTasks } from './store/tasks';
 import store from './store/configureStore';
+
+const gettingTasks = async () => {
+  try {
+    //calling Api
+    const response = await axios.get('http://localhost:5000/api/tasks');
+    console.log(response);
+    // dispatch action
+    store.dispatch(getTasks({ tasks: response.data }));
+  } catch (error) {
+    store.dispatch({ type: 'SHOW_ERROR', payload: { error: error.message } });
+  }
+};
+
+gettingTasks();
 
