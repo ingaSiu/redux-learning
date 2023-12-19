3e80fb4ac418b24c8bd842fe2d85e1eab18e4f83
ingaSiu <ingasiud@gmail.com>
2023-12-19 10:32:32 +0200

    remove action calls

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -3,21 +3,3 @@ import { addTask, completeTask, removeTask } from './store/tasks';
 import { addEmployee } from './store/employess';
 import store from './store/configureStore';
 
-// const unsubscribe = store.subscribe(() => {
-//   console.log('Updated', store.getState());
-// });
-
-// store.dispatch(addTask({ task: 'Task 1' }));
-// store.dispatch(addTask({ task: 'Task 2' }));
-// console.log(store.getState());
-
-// // unsubscribe();
-// store.dispatch(completeTask({ id: 2 }));
-
-// store.dispatch(removeTask({ id: 1 }));
-// // store.dispatch(fetchTodo());
-// console.log(store.getState());
-
-store.dispatch(addEmployee({ name: 'Yuuta' }));
-store.dispatch({ type: 'SHOW_ERROR', payload: { error: 'User not found' } });
-
