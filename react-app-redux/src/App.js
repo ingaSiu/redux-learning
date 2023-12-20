340006768a9fb76e72263ad09e4e535e8b7d5585
ingaSiu <ingasiud@gmail.com>
2023-12-20 07:07:47 +0200

    create and wrap in context

--- react-app-redux/src/App.js
+++ react-app-redux/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import StoreContext from './contexts/storeContext';
+import Tasks from './components/Tasks';
+import store from './store/configureStore';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreContext.Provider value={store}>
+      <div>
+        <Tasks />
+      </div>
+    </StoreContext.Provider>
   );
-}
+};
 
 export default App;
+
