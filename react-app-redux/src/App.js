ce5ce2f751061c7387bb8e1cf6c2d1bb6f523d57
ingaSiu <ingasiud@gmail.com>
2023-12-20 07:26:44 +0200

    remove context, add react-redux provider instead

--- react-app-redux/src/App.js
+++ react-app-redux/src/App.js
@@ -1,14 +1,10 @@
-import StoreContext from './contexts/storeContext';
 import Tasks from './components/Tasks';
-import store from './store/configureStore';
 
 const App = () => {
   return (
-    <StoreContext.Provider value={store}>
-      <div>
-        <Tasks />
-      </div>
-    </StoreContext.Provider>
+    <div>
+      <Tasks />
+    </div>
   );
 };
 
